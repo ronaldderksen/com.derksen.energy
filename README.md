3efadebdb86deda2f732a91fd5373ca2001651d1
Ronald Derksen <ronald@derksen-it.nl>
2023-06-28 14:58:42 +0200

    Version 0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# Enever prijzenfeed
+# Dynamic electricity price
 
-Empty app
+### This homey app is still in testing. Do not rely on correct behaviour yet.
+
+There are  2 triggers:
+1. electricity price lower than:
+2. electricity price higher than:
+
+Every hour the electricity prices are checked and a trigger is triggered when a condition is met. If the the trigger is still met the next hour, no new trigger is triggered. During app start triggers are tested and triggered.
+Each trigger returns a 'token' containing the current electricity price.
+
+## Installation
+
+Please read https://apps.developer.homey.app/ to get started with homey app development before trying out this app.
+
+```
+git clone https://github.com/ronaldderksen/com.derksen.energy.git
+cd com.derksen.energy
+npm install
+homey app install
+```
+
+## Enever prijzenfeeds
+This app uses the Enever prijzenfeeds, see https://enever.nl/prijzenfeeds/ You need to register for a token.
+During app startup 1 call is done and after that 1 call each day. So this app is doing its best to stay within the requests limits. But no guarantee, use at your own risk.
+
+## Example
+[![Flow](https://flow-api.athom.com/api/flow/O-RFdA/image)](https://homey.app/f/O-RFdA/)
--- app.js
+++ app.js
@@ -1,17 +1,196 @@
 'use strict';
 
 const Homey = require('homey');
-const fetch = require('node-fetch');
+const http = require('http.min');
+const cron = require('node-cron');
+const luxon = require('luxon');
 
-class MyApp extends Homey.App {
+//const testMode = true
 
-  /**
-   * onInit is called when the app is initialized.
-   */
+class DerksenEnergy extends Homey.App {
+
+  async refreshToday() {
+    this.log('refreshToday');
+    this.log('token=', this.token);
+
+    if (this.token) {
+      const thisSave = this;
+      http('https://enever.nl/api/stroomprijs_vandaag.php?token=' + this.token).then(function (result) {
+        thisSave.log('Code: ' + result.response.statusCode);
+        if ( result.response.statusCode == 200 ) {
+          const data = JSON.parse(result.data);
+          if (data.data.length == 24) {
+            thisSave.stroomprijs_vandaag = data.data;
+          }
+          thisSave.doTrigger();
+        }
+      })
+    }
+  }
+
+  async refreshTomorrow() {
+    this.log('refreshTomorrow');
+    if (this.token) {
+      const thisSave = this;
+      http('https://enever.nl/api/stroomprijs_morgen.php?token=' + this.token).then(function (result) {
+        thisSave.log('Code: ' + result.response.statusCode);
+        if ( result.response.statusCode == 200 ) {
+          const data = JSON.parse(result.data);
+          if (data.data.length == 24) {
+            thisSave.stroomprijs_morgen = data.data;
+          }
+        }
+      })
+    }
+  }
+
+  extractElectricityPrice() {
+    var ret = null;
+    const key = 'prijs' + this.supplier.toUpperCase();
+
+    if (this.stroomprijs_vandaag != null) {
+      if (typeof testMode !== 'undefined') {
+        if (!this.index) {
+          this.index = 0;
+        }
+        var elm = this.stroomprijs_vandaag[this.index];
+        ret = parseFloat(this.stroomprijs_vandaag[this.index][key]);
+        this.log('datum=', elm.datum, 'price=' + ret, 'index=' + this.index);
+        this.index = this.index + 1;
+        if (this.index > 23) {
+          process.exit(0);
+          this.index = 0;
+        }
+      } else {
+        const ts = luxon.DateTime.now().setZone("Europe/Amsterdam").toFormat('yyyy-MM-dd HH:00:00');
+        this.log(ts);
+        for (const elm of this.stroomprijs_vandaag) {
+          if (elm.datum == ts) {
+            if (elm[key]) {
+              this.log(elm[key]);
+              ret = parseFloat(elm[key]);
+              break;
+            }
+          }
+        }
+      }
+    }
+    return ret;
+  }
+
+  currentHour() {
+    return parseFloat(luxon.DateTime.now().setZone("Europe/Amsterdam").toFormat('HH'));
+  }
+
+  async doTrigger() {
+    this.log('doTrigger');
+    if (this.currentHour() === 0 && this.stroomprijs_morgen != null) {
+      this.stroomprijs_vandaag = this.stroomprijs_morgen;
+      this.stroomprijs_morgen = null;
+    }
+    var currentElectricityPrice = this.extractElectricityPrice();
+    if (currentElectricityPrice) {
+      this.electricityPriceLowerThanTrigger.trigger( { "electricity_price": currentElectricityPrice, "prev_price": this.prevElectricityPrice },
+                                               { 'electricity_price': currentElectricityPrice, "prev_price": this.prevElectricityPrice } );
+
+      this.electricityPriceHigherThanTrigger.trigger( { "electricity_price": currentElectricityPrice, "prev_price": this.prevElectricityPrice },
+                                                { 'electricity_price': currentElectricityPrice, "prev_price": this.prevElectricityPrice } );
+
+      this.prevElectricityPrice = currentElectricityPrice
+    }
+    if (this.currentHour() > 15 && this.stroomprijs_morgen == null) {
+      this.refreshTomorrow();
+    }
+  }
+
   async onInit() {
-    this.log('com.derksen.enever has been initialized');
+    this.log('App has been initialized');
+
+    const thisSave = this;
+    this. prevElectricityPrice = null;
+    this.stroomprijs_vandaag = null;
+    this.stroomprijs_morgen = null;
+    this.token = this.homey.settings.get('token');
+    this.supplier = this.homey.settings.get('supplier');
+
+    this.refreshToday();
+
+    this.homey.settings.on("set", (key) => {
+      this.log('setting.on set ' + key);
+      if (key === 'token') {
+        this.token = this.homey.settings.get('token');
+      } else if (key === 'supplier') {
+        this.supplier = this.homey.settings.get('supplier');
+      }
+      this.refreshToday();
+    })
+
+    // Lower
+    this.electricityPriceLowerThanTrigger = this.homey.flow.getTriggerCard('electricity_price_lower_than');
+
+    this.electricityPriceLowerThanTrigger.on("update", () => {
+      this.log("electricityPriceLowerThanTrigger update");
+      this.electricityPriceLowerThanTrigger.getArgumentValues().then((args) => {
+        thisSave.log(args);
+      });
+    });
+
+    this.electricityPriceLowerThanTrigger.registerRunListener(async (args, state) => {
+      this.log("registerRunListener");
+      thisSave.log('args',args);
+      thisSave.log('state',state);
+      var ret = false;
+      if (state.prev_price === null && state.electricity_price < args.electricity_price) {
+        ret = true
+      } else if (state.prev_price !== null && state.electricity_price < args.electricity_price && state.prev_price >= args.electricity_price) {
+        ret = true;
+      }
+      if (ret) {
+        thisSave.log('electricityPriceLowerThanTrigger TRUE args=',args,'state=',state);
+      }
+      return ret;
+    });
+
+    // Higher
+    this.electricityPriceHigherThanTrigger = this.homey.flow.getTriggerCard('electricity_price_higher_than');
+
+    this.electricityPriceHigherThanTrigger.on("update", () => {
+      this.log("electricityPriceHigherThanTrigger update");
+      this.electricityPriceHigherThanTrigger.getArgumentValues().then((args) => {
+        thisSave.log(args);
+      });
+    });
+
+    this.electricityPriceHigherThanTrigger.registerRunListener(async (args, state) => {
+      this.log("registerRunListener");
+      thisSave.log('args',args);
+      thisSave.log('state',state);
+      var ret = false;
+      if (state.prev_price === null && state.electricity_price > args.electricity_price) {
+        ret = true
+      } else if (state.prev_price !== null && state.electricity_price > args.electricity_price && state.prev_price <= args.electricity_price) {
+        ret = true;
+      }
+      if (ret) {
+        thisSave.log('electricityPriceHigherThanTrigger TRUE args=',args,'state=',state);
+      }
+      return ret;
+    });
+
+    if (typeof testMode !== 'undefined') {
+      const interval = setInterval(function() {
+        thisSave.log('setInterval executed');
+        thisSave.doTrigger();
+      }, 1000);
+    } else {
+      cron.schedule('0 * * * *', async () => {
+        thisSave.log('Schedule executed');
+        thisSave.doTrigger();
+      });  
+    }
+
   }
 
 }
 
-module.exports = MyApp;
+module.exports = DerksenEnergy;
